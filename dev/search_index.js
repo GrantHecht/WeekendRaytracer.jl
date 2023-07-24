e47d2da194fab43d64021e0e7383b78984135c8d
Documenter.jl <documenter@juliadocs.github.io>
2023-07-24 00:06:53 +0000

    build based on 3592756

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WeekendRaytracer","category":"page"},{"location":"#WeekendRaytracer","page":"Home","title":"WeekendRaytracer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WeekendRaytracer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WeekendRaytracer]","category":"page"}]
+}
